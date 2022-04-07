6ea09ef9128c9f83a97f4e4c98f11d210ca95d1a
test <test@qq.com>
2022-04-07 22:17:54 +0800

    上传本地项目

---
+++ src/api/category.js
@@ -0,0 +1,65 @@
+import request from '@/utils/request'
+
+export function fetchCategoryList() {
+  return request({
+    url: '/category/list',
+    method: 'get'
+  })
+}
+
+//
+// export function login(data) {
+//   return request({
+//     url: '/vue-admin-template/user/login',
+//     method: 'post',
+//     data
+//   })
+// }
+//
+// export function logout() {
+//   return request({
+//     url: '/admin/info/logout',
+//     method: 'delete'
+//   })
+// }
+//
+// export function getInfo(token) {
+//   return request({
+//     url: '/admin/info',
+//     method: 'get',
+//     params: { token }
+//   })
+// }
+
+//
+// export function fetchArticle(id) {
+//   return request({
+//     url: '/vue-element-admin/article/detail',
+//     method: 'get',
+//     params: { id }
+//   })
+// }
+//
+// export function fetchPv(pv) {
+//   return request({
+//     url: '/vue-element-admin/article/pv',
+//     method: 'get',
+//     params: { pv }
+//   })
+// }
+//
+// export function createArticle(data) {
+//   return request({
+//     url: '/vue-element-admin/article/create',
+//     method: 'post',
+//     data
+//   })
+// }
+//
+// export function updateArticle(data) {
+//   return request({
+//     url: '/vue-element-admin/article/update',
+//     method: 'post',
+//     data
+//   })
+// }
