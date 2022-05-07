12fe44b8655b28125886b58266880f1e3d12efb1
test <test@qq.com>
2022-05-07 17:27:32 +0800

    优化帖子详情样式

--- src/views/posts/index.vue
+++ src/views/posts/index.vue
@@ -138,19 +138,15 @@
 
     <pagination v-show="total>0" :total="total" :page.sync="listQuery.pageNum" :limit.sync="listQuery.pageSize" @pagination="getList" />
     <el-dialog :visible.sync="dialogDetailVisible" title="帖子详情">
-      <div>
-        <p v-html="postDetail" />
-      </div>
+      <div class="postStyle" v-html="postDetail" />
     </el-dialog>
   </div>
 </template>
 
 <script>
-import { fetchPv, createArticle, updateArticle } from '@/api/admin'
 import { fetchCategoryList } from '@/api/category'
 import { deletePostByAdmin, fetchPostList, lockedPost, unLockPost } from '@/api/post'
 import waves from '@/directive/waves' // waves directive
-import { parseTime } from '@/utils'
 import Pagination from '@/components/Pagination' // secondary package based on el-pagination
 
 export default {
@@ -335,3 +331,42 @@ export default {
   }
 }
 </script>
+<style  lang="less" scoped>
+
+//帖子详情的样式
+.postStyle {
+/* table 样式 */
+/deep/ table {border-top: 1px solid #ccc;
+  border-left: 1px solid #ccc;
+}
+/deep/ table td,
+/deep/ table th {
+  border-bottom: 1px solid #ccc;
+  border-right: 1px solid #ccc;
+  padding: 3px 5px;
+}
+/deep/ table th {border-bottom: 2px solid #ccc;text-align: center;}
+/* blockquote 样式 */
+/deep/  blockquote {
+  display: block;border-left: 8px solid #d0e5f2;padding: 5px 10px;
+  margin: 10px 0;line-height: 1.4;font-size: 100%;background-color: #f1f1f1;}
+/* code 样式 */
+/deep/ code {
+  display: inline-block;
+  *display: inline;
+  *zoom: 1;
+  background-color: #f1f1f1;
+  border-radius: 3px;
+  padding: 3px 5px;
+  margin: 0 3px;
+}
+/deep/ pre code {
+  display: block;
+}
+
+/* ul ol 样式 */
+/deep/ ul, ol {
+  margin: 10px 0 10px 20px;
+}
+}
+</style>
